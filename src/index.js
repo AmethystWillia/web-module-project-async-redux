a637874a748a0ebc46aafe7a0283c2d60a017ad5
Amethyst Willia <zmithic@gmail.com>
2021-12-08 14:34:51 -0600

    Connected redux to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Import redux stuff
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+// Import reducers
+import { catReducer } from './reducers/catReducer';
+
+// Create store
+const store = createStore(catReducer, applyMiddleware(thunk, logger));
 
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
