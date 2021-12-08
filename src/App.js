d92b6108c7381d2314c629925bb9ff35daa6801f
Amethyst Willia <zmithic@gmail.com>
2021-12-08 14:18:57 -0600

    Made folders for components, reducers, and actions; added a very basic Cat component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+// Import component
+import Cat from './components/Cat';
+
 function App() {
   return (
     <div className="App">
       Async Redux Project
+      <Cat />
     </div>
   );
 }
---
+++ src/components/Cat.js
@@ -0,0 +1,13 @@
+// ---------- Imports ---------- //
+import React from 'react';
+
+const Cat = (props) => {
+    return (
+        <div>
+            <h2>Generate a Cat!</h2>
+            <button>Is a Button</button>
+        </div>
+    );
+};
+
+export default Cat;
